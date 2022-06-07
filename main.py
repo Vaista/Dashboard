0ba8428581de56a21a64d7c88d546b62bc7fba03
Vaibhav Sharma <93530649+Vaista@users.noreply.github.com>
2022-06-07 13:20:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1")
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
